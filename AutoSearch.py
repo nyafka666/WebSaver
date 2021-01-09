57da6ec94944568d66af670d1fe35f3ffbb9019c
XmlEntity <netberry@tutanota.com>
2021-01-09 17:37:02 +0200

    search function

--- AutoSearch.py
+++ AutoSearch.py
@@ -1,1 +1,15 @@
+import requests as req
+import sys
 
+query = sys.argv[1]
+engine = sys.argv[2]
+
+
+def search(query, engine):
+    engines = {'ddg': 'https://duckduckgo.com/?q='}
+    url_query = engines[engine] + query
+    response = req.get(url_query).text
+    return url_query
+
+
+print(search(query, engine))
